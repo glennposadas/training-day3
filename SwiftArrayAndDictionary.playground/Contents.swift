4aeb1a39b5556c5172be5ddbde3d6d535857e59b
Glenn Posadas <hello@glennvon.com>
2022-01-07 09:54:35 +0800

    add array print

--- SwiftArrayAndDictionary.playground/Contents.swift
+++ SwiftArrayAndDictionary.playground/Contents.swift
@@ -1,3 +1,36 @@
 import UIKit
 
-var greeting = "Hello, playground"
+// ARRAYS
+// NSArray and NSMutableArray
+// array
+
+
+// Swift
+
+let numbers: [Int] = [1, 2, 3]
+
+print(numbers)
+
+//for currentNumber in numbers {
+//  print("Current number is: ----> \(currentNumber)")
+//}
+
+
+
+//
+//
+//
+//// For loop
+//
+//numbers.forEach { currentNumber in
+//  print(currentNumber)
+//}
+//
+//// Map
+//numbers.map { currentNumber in
+//  let numberString = "\(currentNumber)"
+////  let myString = "The current is: " + numberString + " bye!!!!!" + " 12312312 "
+//  let anotherMyString = "Hey number \(numberString)"
+//
+//  print(anotherMyString)
+//}
